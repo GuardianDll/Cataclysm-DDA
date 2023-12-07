662e7f7239a1d79754277f56311195b7f1a6e59e
Antti Riikonen <harava@gmail.com>
2023-12-07 16:39:32 +0000

    Use pocketless welder in tests

--- tests/vehicle_interact_test.cpp
+++ tests/vehicle_interact_test.cpp
@@ -96,9 +96,6 @@ TEST_CASE( "repair_vehicle_part", "[vehicle]" )
         // tools.push_back( tool_with_ammo( "welder", 10000 ) );
 
         item welder( "welder" );
-        item welder_mag( welder.magazine_default() );
-        welder_mag.ammo_set( welder_mag.ammo_default(), 1000 );
-        welder.put_in( welder_mag, pocket_type::MAGAZINE_WELL );
         tools.push_back( welder );
 
         tools.emplace_back( "goggles_welding" );
@@ -129,9 +126,6 @@ TEST_CASE( "repair_vehicle_part", "[vehicle]" )
         std::vector<item> tools;
 
         item welder( "welder" );
-        item welder_mag( welder.magazine_default() );
-        welder_mag.ammo_set( welder_mag.ammo_default(), 1000 );
-        welder.put_in( welder_mag, pocket_type::MAGAZINE_WELL );
         tools.push_back( welder );
 
         tools.emplace_back( "hammer" );
@@ -143,9 +137,6 @@ TEST_CASE( "repair_vehicle_part", "[vehicle]" )
         std::vector<item> tools;
 
         item welder( "welder" );
-        item welder_mag( welder.magazine_default() );
-        welder_mag.ammo_set( welder_mag.ammo_default(), 500 );
-        welder.put_in( welder_mag, pocket_type::MAGAZINE_WELL );
         tools.push_back( welder );
 
         tools.emplace_back( "goggles_welding" );
@@ -175,9 +166,6 @@ TEST_CASE( "repair_vehicle_part", "[vehicle]" )
         std::vector<item> tools;
 
         item welder( "welder" );
-        item welder_mag( welder.magazine_default() );
-        welder_mag.ammo_set( welder_mag.ammo_default(), 500 );
-        welder.put_in( welder_mag, pocket_type::MAGAZINE_WELL );
         tools.push_back( welder );
 
         tools.emplace_back( "goggles_welding" );
